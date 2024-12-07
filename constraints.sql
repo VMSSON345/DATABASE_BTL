d6aa5e85dbba143bf298fdeb1fd4633dde4b5333
VMSSON345 <23020424@vnu.edu.vn>
2024-12-07 21:52:00 +0700

    commit

--- constraints.sql
+++ constraints.sql
@@ -7,9 +7,9 @@ ADD CONSTRAINT CK_GIA_MH CHECK (GIA_MH >= 0);
 ALTER TABLE CHI_TIET_HOA_DON
 ADD CONSTRAINT CK_SO_LUONG CHECK (SO_LUONG >= 0);
 
--- Constraint 3: Không cho phép giá bán trong chi tiết hóa đơn âm
-ALTER TABLE CHI_TIET_HOA_DON
-ADD CONSTRAINT CK_GIA_BAN CHECK (GIA_BAN >= 0);
+-- Constraint 3: Địa chỉ khách hàng không được vượt quá 100 ký tự
+ALTER TABLE KHACH_HANG
+ADD CONSTRAINT CK_DIA_CHI_LENGTH CHECK (LENGTH(DIA_CHI) <= 100);
 
 -- Constraint 4: Số điện thoại nhân viên phải có đúng 10 ký tự số
 ALTER TABLE NHAN_VIEN
@@ -39,6 +39,4 @@ ADD CONSTRAINT CK_TEN_MH_NOT_EMPTY CHECK (LENGTH(TRIM(TEN_MH)) > 0);
 ALTER TABLE NHAN_VIEN
 ADD CONSTRAINT CK_TEN_NV_NOT_EMPTY CHECK (LENGTH(TRIM(TEN_NV)) > 0);
 
--- Constraint 11: Địa chỉ khách hàng không được vượt quá 100 ký tự
-ALTER TABLE KHACH_HANG
-ADD CONSTRAINT CK_DIA_CHI_LENGTH CHECK (LENGTH(DIA_CHI) <= 100);
+
