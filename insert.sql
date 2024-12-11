28a13450c67deb328cf8bfbbf53c8a316d18e078
VMSSON345 <23020424@vnu.edu.vn>
2024-12-11 21:15:18 +0700

    insert

--- insert.sql
+++ insert.sql
@@ -62,7 +62,7 @@ INSERT INTO MAT_HANG (MA_MH, TEN_MH, GIA_MH, SL_TONKHO, MA_NHOM) VALUES
 ('MH028', 'Khóa học online', 500000, 150, 'N028'),
 ('MH029', 'Vé xem phim', 100000, 500, 'N029'),
 ('MH030', 'Đặt phòng khách sạn', 1000000, 80, 'N030'),
-('MH031', 'Lego', 500000, 100, 'N017'),
+('MH031', 'Lego', 500000, 3000, 'N017'),
 ('MH032', 'Chảo', 400000, 100, 'N005'),
 ('MH033', 'Xoong', 300000, 200, 'N005'),
 ('MH034', 'Thìa', 20000, 3000, 'N005'),
